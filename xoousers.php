0d6351b28b281d2790681b56331ee43aa8980ad2
UsersUltra <UsersUltra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-14 20:13:02 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/users-ultra/trunk@965990 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- xoousers.php
+++ xoousers.php
@@ -3,7 +3,7 @@
 Plugin Name: Users Ultra Lite
 Plugin URI: http://usersultra.com
 Description: This is a powerful user profiles plugin for WordPress.
-Version: 1.3.11
+Version: 1.3.12
 Author: Users Ultra
 Author URI: http://usersultra.com
 */
